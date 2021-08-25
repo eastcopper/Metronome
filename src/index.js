b85f06aca65fe2992d6abc953fcd35786c07b445
ldh7228 <ldh7228@gmail.com>
2021-08-25 19:39:18 +0900

    [Add] setInterval

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 const bpmValue = document.getElementById('bpmValue');
 const input = document.getElementById('input');
 
-let inputValue;
+let inputValue = 60;
 let sound = new Audio("../asset/sound.wav");
 
 function range() {
     inputValue = document.getElementById('input').value;
     bpmValue.innerHTML = `${inputValue} BPM`;
 }
+
+setInterval(function() {
+    
+}, 60000 / inputValue);
 
 function playSound() {
     sound.currentTime = 0;
