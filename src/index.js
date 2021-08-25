344e2d9d137156902058779b632c746feb5b6a0c
ldh7228 <ldh7228@gmail.com>
2021-08-25 20:10:39 +0900

    Add start button click event

--- src/index.js
+++ src/index.js
@@ -1,26 +1,35 @@
 const bpmValue = document.getElementById('bpmValue');
 const input = document.getElementById('input');
-const click = document.getElementById('start');
+const start = document.getElementById('start');
 
 let time = null;
 let inputValue = 60;
 let sound = new Audio("../asset/sound.wav");
+let play = false;
 
 function range() {
     inputValue = document.getElementById('input').value;
     bpmValue.innerHTML = `${inputValue} BPM`;
-    clearInterval(time);
-    time = setInterval(playSound, realBpm(inputValue));
+    if (play) {
+        clearInterval(time);
+        time = setInterval(playSound, realBpm(inputValue));
+    }
 }
 
 function realBpm(bpm) {
     return (60 * 1000) / bpm;
- }
+}
 
 function playSound() {
     sound.currentTime = 0;
     sound.volume = 0.3;
     sound.play();
- }
+}
+
+function bpmStart() {
+    play = true;
+    time = setInterval(playSound, realBpm(inputValue));
+}
 
 input.addEventListener("input", range);
+start.addEventListener("click", bpmStart);
