1cd6229e1c35b31e86a97ec19d5dfc67c430fa42
ldh7228 <ldh7228@gmail.com>
2021-08-25 20:07:24 +0900

    Fix bpm

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 const bpmValue = document.getElementById('bpmValue');
 const input = document.getElementById('input');
+const click = document.getElementById('start');
 
+let time = null;
 let inputValue = 60;
 let sound = new Audio("../asset/sound.wav");
 
 function range() {
     inputValue = document.getElementById('input').value;
     bpmValue.innerHTML = `${inputValue} BPM`;
+    clearInterval(time);
+    time = setInterval(playSound, realBpm(inputValue));
 }
 
-setInterval(function() {
-    playSound();
-}, 60000 / inputValue);
+function realBpm(bpm) {
+    return (60 * 1000) / bpm;
+ }
 
 function playSound() {
     sound.currentTime = 0;
-    sound.volume = 0.5;
+    sound.volume = 0.3;
     sound.play();
  }
 
