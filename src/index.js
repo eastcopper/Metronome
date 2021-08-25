7937725dc1a7c6b2cf976fe73ab7e2d49f39ed48
ldh7228 <ldh7228@gmail.com>
2021-08-25 19:31:42 +0900

    Add audio

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const bpmValue = document.getElementById('bpmValue');
 const input = document.getElementById('input');
 
+let inputValue;
+let sound = new Audio("../asset/sound.wav");
+
 function range() {
-    let inputValue = document.getElementById('input').value;
+    inputValue = document.getElementById('input').value;
     bpmValue.innerHTML = `${inputValue} BPM`;
 }
+
+function playSound() {
+    sound.currentTime = 0;
+    sound.play();
+ }
 
 input.addEventListener("input", range);
